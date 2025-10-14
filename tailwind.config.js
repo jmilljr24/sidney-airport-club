2b39f4ab89471afad079f5ddaa3a590326e8d8f3
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2025-10-13 20:20:14 -0400

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+// module.exports = {
+//   content: [
+//     './_drafts/**/*.html',
+//     './_includes/**/*.html',
+//     './_layouts/**/*.html',
+//     './_posts/*.md',
+//     './*.md',
+//     './*.html',
+//   ],
+//   theme: {
+//     theme: {
+//       extend: {},
+//     },
+//   },
+//   plugins: []
+// }
+
+module.exports = {
+  content: [
+    "./_drafts/**/*.html",
+    "./_includes/**/*.html",
+    "./_includes/**/*.liquid",
+    "./_layouts/**/*.html",
+    "./_layouts/**/*.liquid",
+    "./_posts/*.md",
+    "./*.md",
+    "./*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
